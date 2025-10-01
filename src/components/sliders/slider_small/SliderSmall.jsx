670df8677fc79a82496f831a1ffbb5e9d44d9a41
Aarni <aarni.fangmayer@gmail.com>
2025-10-02 00:09:34 +0300

    feat: add card switching logic (prev/next) without animation for component SliderSmall

--- src/components/sliders/slider_small/SliderSmall.jsx
+++ src/components/sliders/slider_small/SliderSmall.jsx
@@ -1,22 +1,96 @@
-import React from 'react'
-import SliderButtons from '../../buttons/slider_buttons/SliderButtons'
-import CardsSmall from '../../cards/cards_small/CardsSmall'
-import './sliderSmall.css'
+import React, { useState } from "react";
+import SliderButtons from "../../buttons/slider_buttons/SliderButtons";
+import CardsSmall from "../../cards/cards_small/CardsSmall";
+import "./sliderSmall.css";
 
 const SliderSmall = () => {
-  
-  return (
-    <div className="slider-small">
-      <div className="slider-small__controls">
-        <SliderButtons btnType={"vertical"}/>
-      </div>
-      <div className="slider-small__track">
-        <CardsSmall subtitle={'difficulty: easy'} title={'Three-day expeditions for everyone'} btnText={'Book a tour'} />
-        <CardsSmall subtitle={'difficulty: hard'} title={'Multi-day expeditions with camping '} btnText={'Book a tour'} />
-        <CardsSmall subtitle={'difficulty: middle'} title={'Abroad routes beyond Canada’s borders '} btnText={'Book a tour'} />
-      </div>
-    </div>
-  )
-}
+  const initialCards = [
+    {
+      id: 1,
+      subtitle: "difficulty: easy",
+      title: "Three-day expeditions for everyone",
+    },
+    {
+      id: 2,
+      subtitle: "difficulty: hard",
+      title: "Multi-day expeditions with camping",
+    },
+    {
+      id: 3,
+      subtitle: "difficulty: middle",
+      title: "Abroad routes beyond Canada’s borders",
+    },
+    {
+      id: 4,
+      subtitle: "difficulty: middle",
+      title: "Abroad routes beyond Canada’s borders",
+    },
+    {
+      id: 1,
+      subtitle: "difficulty: easy",
+      title: "Three-day expeditions for everyone",
+    },
+    {
+      id: 2,
+      subtitle: "difficulty: hard",
+      title: "Multi-day expeditions with camping",
+    },
+    {
+      id: 3,
+      subtitle: "difficulty: middle",
+      title: "Abroad routes beyond Canada’s borders",
+    },
+    {
+      id: 4,
+      subtitle: "difficulty: middle",
+      title: "Abroad routes beyond Canada’s borders",
+    },
+  ];
 
-export default SliderSmall
+  const [cards, setCards] = useState(initialCards);
+
+  const handleNext = () => {
+    setCards((prev) => {
+      const newCards = [...prev];
+      const first = newCards.shift();
+      newCards.push(first);
+      return newCards;
+    });
+  };
+
+  const handlePrev = () => {
+    setCards((prev) => {
+      const newCards = [...prev];
+      const last = newCards.pop();
+      newCards.unshift(last);
+      return newCards;
+    });
+  };
+
+  return (
+    <>
+      <div className="slider-small__controls">
+        <SliderButtons
+          btnType={"vertical"}
+          nextSlide={handleNext}
+          prevSlide={handlePrev}
+        />
+      </div>
+      <div className="slider-small">
+        <div
+          className="slider-small__track"
+        >
+          {cards.map((card) => (
+            <CardsSmall
+              subtitle={card.subtitle}
+              title={card.title}
+              btnText={"Book a tour"}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SliderSmall;
