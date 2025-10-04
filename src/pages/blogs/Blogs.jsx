84cf85cea5e554c351eb392e5a9da341280399c6
Aarni <aarni.fangmayer@gmail.com>
2025-10-04 14:58:43 +0300

    feat: integrate blog page sections into Blogs component

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import StaticLayout from '../../components/layouts/staticLayout/StaticLayout'
+import InfoSectionBlog from './page_sections/InfoSectionBlog'
+import PopularArticlesSectionBlog from './page_sections/PopularArticlesSectionBlog'
+import AllArticlesSectionBlog from './page_sections/AllArticlesSectionBlog'
+import PromoSectionBlog from './page_sections/PromoSectionBlog'
+import './blogs.css'
 
 const Blogs = () => {
   return (
     <StaticLayout>
-      this is blogs
+      <InfoSectionBlog />
+      <PopularArticlesSectionBlog />
+      <AllArticlesSectionBlog />
+      <PromoSectionBlog />
     </StaticLayout>
   )
 }
