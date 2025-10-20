ba9405821e84e27b4f957b1eee10194bbb346d60
Aarni <aarni.fangmayer@gmail.com>
2025-10-20 15:15:24 +0300

    feat: add styles and responsiveness to ArticleCardPromo, ProfileForm, ProfileAvatar, ProfileEditor, TextBlockWithHighlights, and Account components

--- src/components/shared/ProfileEditor.jsx
+++ src/components/shared/ProfileEditor.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileAvatar from "./ProfileAvatar";
 import ProfileForm from "../forms/ProfileForm";
 import "./profileEditor.css";
+import editImage from "../../assets/icons/edit_image.png"
 
 const ProfileEditor = ( { avatarImg } ) => {
+  const [accordion, setAccordion] = useState(false);
   return (
     <div className="account__edit">
-      <h3 className="account__edit-title">Edit profile</h3>
-      <div className="account__edit-content">
-        <ProfileAvatar avatarImg={avatarImg} btnText={"change avatar"} />
-        <ProfileForm placeholderName={"Name Name"} placeholderEmail={"email@gmail.com"} placeholderPhone={"40 856 6545"} btnText={"Save"} />
-      </div>
+      <button className="account__edit-title" onClick={()=> setAccordion(!accordion)}><h3>Edit profile</h3><img src={editImage} alt="edit icon pencil" /></button>
+      {accordion &&
+        <div className="account__edit-content">
+          <ProfileAvatar avatarImg={avatarImg} btnText={"change avatar"} />
+          <ProfileForm placeholderName={"Name Name"} placeholderEmail={"email@gmail.com"} placeholderPhone={"40 856 6545"} btnText={"Save"} />
+        </div>
+      }
+
     </div>
   );
 };
