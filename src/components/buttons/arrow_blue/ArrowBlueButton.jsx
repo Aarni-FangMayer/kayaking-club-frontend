7ba2a35d406d2db8784e92493c532a5e8977ceee
Aarni <aarni.fangmayer@gmail.com>
2025-10-22 15:20:12 +0300

    feat: add modal with full article view and interactive button using BlogsModal and ArticleWithComments

--- src/components/buttons/arrow_blue/ArrowBlueButton.jsx
+++ src/components/buttons/arrow_blue/ArrowBlueButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./arrowBlueButton.css";
 import ArrowBlue from "../../../assets/icons/arrow_blue.png";
 
-const ArrowBlueButton = ( {text} ) => {
+const ArrowBlueButton = ( { clickEvent, text } ) => {
   return (
-    <button className="arrow-blue-button">
+    <button onClick={clickEvent} className="arrow-blue-button">
       {text}
       <img src={ArrowBlue} alt="" />
     </button>
--- src/components/cards/article_card/ArticleCard.jsx
+++ src/components/cards/article_card/ArticleCard.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArrowBlueButton from '../../buttons/arrow_blue/ArrowBlueButton'
+import BlogsModal from '../../modals/blogsModal/BlogsModal'
+import ArticleWithComments from '../../../pages/blogs/ArticleWithComments/ArticleWithComments'
 import './articleCard.css'
 
-const ArticleCard = ({ subtitle, description, btnText }) => {
+const ArticleCard = ({ subtitle, description, image, textContent, btnText }) => {
+  const [blogModalOpen, setBlogModalOpen] = useState(false);
+
+  const closeBlogModal = () => {
+    setBlogModalOpen(false);
+  }
   return (
     <div className="article-card">
         <div className="article-card__content">
             <h3 className="article-card__subtitle">{subtitle}</h3>
             <p className="article-card__description">{description}</p>
         </div>
-        <ArrowBlueButton text={btnText} />
+        <ArrowBlueButton clickEvent={() => setBlogModalOpen(true)} text={btnText} />
+        <BlogsModal isModalOpen={blogModalOpen} closeModal={closeBlogModal}>
+          <ArticleWithComments title={subtitle} text={textContent} image={image} />
+        </BlogsModal>
     </div>
   )
 }
