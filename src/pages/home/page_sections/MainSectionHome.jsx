64109918f2e7a6b7fc87972e72a7aa0b55dfff22
Aarni <aarni.fangmayer@gmail.com>
2025-10-22 15:41:07 +0300

    feat: add login and registration modals with UserAuthModal and SuccessRegistrationModal

--- src/pages/home/page_sections/MainSectionHome.jsx
+++ src/pages/home/page_sections/MainSectionHome.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowBlueButton from "../../../components/buttons/arrow_blue/ArrowBlueButton";
 import SliderSmall from "../../../components/sliders/slider_small/SliderSmall";
+import Modal from "../../../components/modals/modalLayout/Modal";
+import UserAuthModal from "../../../components/modals/userAuthModal/UserAuthModal";
+import SuccessRegistrationModal from "../../../components/modals/successRegistrationModal/SuccessRegistrationModal";
 import './mainSectionHome.css'
 
 const MainSectionHome = () => {
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const [registrationModalOpen, setRegistrationModalOpen] = useState(false);
+  const [newUserRegistered, setNewUserregistered] = useState(false);
+  const closeAllModals = () => {
+      setLoginModalOpen(false);
+      setRegistrationModalOpen(false);
+      setNewUserregistered(false);
+  }
+
+  const loginAccount = () => {
+    console.log('welcome to ypur account');
+    setLoginModalOpen(false);
+  }
+
+  const registerUser = () => {
+    console.log("user has been added to database");
+    setRegistrationModalOpen(false);
+    setNewUserregistered(true);
+  }
   return (
     <section id="main" className="home-main">
       <div className="home-content">
@@ -18,9 +40,12 @@ const MainSectionHome = () => {
             Our tours are designed to fit your pace, your passion, <br />
             and adventure.
           </p>
-          <ArrowBlueButton text={"Join us here"} />
+          <ArrowBlueButton clickEvent={() => setLoginModalOpen(true)} text={"Join us here"} />
         </div>
       </div>
+      <Modal closeModal={closeAllModals} isModalOpen={loginModalOpen} loginAccount={loginAccount} registrationModalOpen={registrationModalOpen} setRegistrationModalOpen={setRegistrationModalOpen} registerUser={registerUser}>
+        {newUserRegistered ? <SuccessRegistrationModal loginAccount={loginAccount} /> : <UserAuthModal />}
+      </Modal>
     </section>
   );
 };
