26a21eb980517a0bd06d6ed0d853f79bca58fbff
Aarni <aarni.fangmayer@gmail.com>
2025-10-30 23:01:02 +0200

    chore: add PrimeReact library

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Account from "./pages/account/Account"
+import { PrimeReactProvider } from "primereact/api";
+
+import Account from "./pages/account/Account";
 import Admin from "./pages/admin/Admin";
 import Blogs from "./pages/blogs/Blogs";
 import Tours from "./pages/tours/Tours";
 import Home from "./pages/home/Home";
-import './App.css'
+
+import "./App.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/account" element={<Account />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/blogs" element={<Blogs />} />
-      <Route path="/tours" element={<Tours />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <PrimeReactProvider>
+      <Routes>
+        <Route path="/account" element={<Account />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </PrimeReactProvider>
   );
 }
 
