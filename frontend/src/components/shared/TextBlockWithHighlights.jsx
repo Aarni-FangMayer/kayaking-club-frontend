d4f2399808ad9a209fe5a9690e0a2526db518579
Aarni <aarni.fangmayer@gmail.com>
2025-11-06 03:55:32 +0200

    feat: integrate toursService and refactor tours logic across components ToursList and TextBlockWithHighlights

--- frontend/src/components/shared/TextBlockWithHighlights.jsx
+++ frontend/src/components/shared/TextBlockWithHighlights.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import toursService from '../../services/tours'
 import "./textBlockWithHighlights.css";
 
 const TextBlockWithHighlights = ({
@@ -9,6 +10,63 @@ const TextBlockWithHighlights = ({
   addRouteBtnText,
   addPostBtnText,
 }) => {
+  const [tours, setTours] = useState([]);
+
+  const [newTour, setNewTour] = useState({
+    name: "",
+    subtitle: "",
+    description: "",
+    included: "",
+    dateOfTrip: "",
+    difficulty: "easy",
+    forBeginners: "",
+    duration: "",
+    price: "",
+    image: "",
+  });
+
+   useEffect(() => {
+    toursService
+      .getAll()
+      .then((response) => setTours(response.data))
+      .catch((error) => console.error("Error loading tours:", error));
+  }, []);
+
+  const handleTourChange = (event) => {
+    const { name, value } = event.target;
+    setNewTour((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const addTour = (event) => {
+    event.preventDefault();
+    console.log("newTour", newTour);
+
+    const tourObject = {
+      id: `tour-${Date.now().toString(36)}`,
+      ...newTour,
+    };
+    toursService.create(tourObject).then(response => {
+      setTours((prev) => prev.concat(response.data))
+      setNewTour({
+      name: "",
+      subtitle: "",
+      description: "",
+      included: "",
+      dateOfTrip: "",
+      difficulty: "easy",
+      forBeginners: "",
+      duration: "",
+      price: "",
+      image: "",
+    });
+    })
+    
+  };
+
+  useEffect(() => {
+  console.log("Tours updated:", tours);
+}, [tours]);
+
   return (
     <div className="account__intro">
       <h2 className="account__greeting">{title}</h2>
@@ -17,9 +75,72 @@ const TextBlockWithHighlights = ({
         <p className="account__description">{describtion}</p>
         <p className="account__highlight">{highlight}</p>
       </div>
+      <form onSubmit={addTour}>
+        <input
+          name="name"
+          type="text"
+          value={newTour.name}
+          onChange={handleTourChange}
+          placeholder="tour name"
+        />
+        <input
+          type="text"
+          name="subtitle"
+          value={newTour.subtitle}
+          onChange={handleTourChange}
+          placeholder="tour subtitle"
+        />
+        <input
+          type="text"
+          name="description"
+          value={newTour.description}
+          onChange={handleTourChange}
+          placeholder="tour description"
+        />
+        <input
+          type="text"
+          name="included"
+          value={newTour.included}
+          onChange={handleTourChange}
+          placeholder="what included"
+        />
+        <input type="text" name="dateOfTrip" value={newTour.dateOfTrip} onChange={handleTourChange} placeholder="tour date" />
+        <select name="difficulty" value={newTour.difficulty} onChange={handleTourChange}>
+          <option value="hard">hard</option>
+          <option value="middle">middle</option>
+          <option value="easy">easy</option>
+        </select>
+        <span>Is it beginners friendely?</span>
+        <label htmlFor="">
+          <input
+            type="radio"
+            name="forBeginners"
+            value="yes"
+            checked={newTour.forBeginners === "yes"}
+            onChange={handleTourChange}
+          />
+          yes
+        </label>{" "}
+        <label htmlFor="">
+          <input
+            type="radio"
+            name="forBeginners"
+            value="no"
+            checked={newTour.forBeginners === "no"}
+            onChange={handleTourChange}
+          />
+          no
+        </label>
+        <input type="text" name="duration" value={newTour.duration} onChange={handleTourChange} placeholder="duration" />
+        <input type="number" name="price" value={newTour.price} onChange={handleTourChange} placeholder="price" />
+        <input type="text" name="image" value={newTour.image} onChange={handleTourChange} placeholder="image" />
+        <button type="submit">create new route</button>
+      </form>
       <div className="account__buttons">
         {addRouteBtnText && (
-          <button className="admin-btn add-routes-btn">{addRouteBtnText}</button>
+          <button className="admin-btn add-routes-btn">
+            {addRouteBtnText}
+          </button>
         )}
         {addPostBtnText && (
           <button className="admin-btn add-post-btn">{addPostBtnText}</button>
--- frontend/src/pages/tours/toursCatalog/ToursList.jsx
+++ frontend/src/pages/tours/toursCatalog/ToursList.jsx
@@ -1,17 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import toursService from "../../../services/tours";
 import SelectedTour from "./SelectedTour";
 import ToursFilterPanel from "./ToursFilterPanel";
 import CatalogTourCard from "../../../components/cards/catalog_tour_card/CatalogTourCard";
 import "./toursList.css";
-// import ImageTour1 from "../../../assets/images/tourImage1.jpg";
-// import ImageTour2 from "../../../assets/images/tourImage2.jpg";
-// import ImageTour3 from "../../../assets/images/tourImage3.jpg";
-// import ImageTour4 from "../../../assets/images/tourImage4.jpg";
-// import ImageTour5 from "../../../assets/images/tourImage5.jpg";
-// import ImageTour6 from "../../../assets/images/tourImage6.jpg";
-// import ImageTour7 from "../../../assets/images/tourImage7.jpg";
-// import ImageTour8 from "../../../assets/images/tourImage8.jpg";
 
 const ToursList = ({
   selectedTour,
@@ -19,205 +11,15 @@ const ToursList = ({
   singleDayTitle,
   multiDayTitle,
 }) => {
-
   const [tourList, setTourList] = useState([]);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/tours").then((response) => {
-      // const tours = response.data;
-      console.log("promise fulfilled");
-      setTourList(response.data)
+    toursService.getAll().then((response) => {
+      setTourList(response.data);
     });
   }, []);
 
-  console.log('render', tourList, 'tourList')
-
-  // const allTours = [
-  //   {
-  //     id: 1,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour1,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-
-  //   {
-  //     id: 2,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour2,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 3,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour3,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 4,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour4,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 5,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour5,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-
-  //   {
-  //     id: 6,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour6,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 7,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour7,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 8,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour8,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 5,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour1,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-
-  //   {
-  //     id: 6,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour2,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 7,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour3,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  //   {
-  //     id: 8,
-  //     tourName: "Lake Louise Paddle",
-  //     subtitle:
-  //       "A classic route through one of Canada’s most iconic lakes — short, peaceful, and endlessly photogenic.",
-  //     description:
-  //       "Paddle across the iconic turquoise waters of Lake Louise, surrounded by soaring mountain peaks and glaciers. This short route is one of the most photographed in Canada — and for good reason. With calm waters and minimal effort, you'll glide through breathtaking scenery, perfect for beginners or anyone looking for a peaceful, picture-perfect day on the water.",
-  //     images: ImageTour4,
-  //     difficulty: "easy",
-  //     dateOfTrip: "28 august",
-  //     included: "guide, photography",
-  //     level: "yes",
-  //     duration: "1 day",
-  //     price: "1050",
-  //   },
-  // ];
+  console.log("render", tourList, "tourList");
 
   const [currentTour, setCurrentTour] = useState({});
 
