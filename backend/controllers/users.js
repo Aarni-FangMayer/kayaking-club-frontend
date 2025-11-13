f71648d459afd16dd9f8f86df3b5804608920af0
Aarni <aarni.fangmayer@gmail.com>
2025-11-14 00:48:58 +0200

    refactor: update users router to use authenticate middleware and promise chaining with error handling

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,29 +1,40 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
+const { authenticate } = require("../utils/middleware"); // JWT или сессия
 
-usersRouter.post("/", async (request, response) => {
-  const { email, password, phone, username } = request.body;
+/* Create new user */
+usersRouter.post("/", async (request, response, next) => {
+  const { email, password, phone, username, role } = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
-
-  const user = new User({
-    email,
-    phone,
-    username,
-    passwordHash,
-  });
-
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
+  bcrypt
+    .hash(password, 10)
+    .then((passwordHash) => {
+      const user = new User({ email, phone, username, passwordHash, role });
+      return user.save();
+    })
+    .then((savedUser) => response.status(201).json(savedUser))
+    .catch((error) => next(error));
 });
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
-})
+/* Fetch all users */
+usersRouter.get("/", async (request, response, next) => {
+  User.find({})
+    .then((users) => response.json(users))
+    .catch((error) => next(error));
+});
 
+// Fetch the user's booked tours
+usersRouter.get("/me/tours", authenticate, (request, response, next) => {
+  User.findById(request.user.id)
+    .populate("tours_id")
+    .then((user) => {
+      if (!user) {
+        return response.status(404).json({ error: "User not found" });
+      }
+      response.json(user.tours_id);
+    })
+    .catch((error) => next(error));
+});
 
 module.exports = usersRouter;
